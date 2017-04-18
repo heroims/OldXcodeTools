e3c775a67e5f04ff0cc6cb4f4b5033fb1fe8d0cf
zhaoyiqi <heroims@163.com>
2017-04-18 11:46:34 +0800

    add shell

---
+++ installOldXcodeTool.sh
@@ -0,0 +1,8 @@
+myFile="PackageApplication"
+xcodePath="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/"
+if [ ! -f "$myFile" ]; then
+    curl -O "https://github.com/heroims/OldXcodeTools/PackageApplication"
+fi
+
+eval "sudo mv $myFile $xcodePath"
+sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer/
